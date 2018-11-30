566bcd4b08c6ca01c18e150020e2fc0bacd8e37f
Emeka Okoronkwo <okoronkwoemeka24@gmail.com>
2018-11-29 20:01:14 -0500

    something

--- Project2.0/src/main/java/com/revature/controllers/ExerciseController.java
+++ Project2.0/src/main/java/com/revature/controllers/ExerciseController.java
@@ -3,9 +3,9 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,12 +31,12 @@ public class ExerciseController {
 		return exerciseService.saveExercise(exercises);
 	}
 	
-	@GetMapping("")
+	@PostMapping("calendar")
 	public List<Exercise> getExercises(@RequestBody User user) {
 		return exerciseService.getExercises(user);
 	}
 	
-	@DeleteMapping("")
+	@PutMapping("")
 	public Exercise deleteExercise(@RequestBody Exercise exercise) {
 		return exerciseService.deleteExercise(exercise);
 	}
